77bba05a2f0de319796f0622dabd6b899394a2cf
Andrew Karpisz <akarpisz4269@gmail.com>
2020-09-16 22:47:56 -0400

    Updates

---
+++ precache-manifest.f4ad42ab471df38d0202ed5f48bb67e1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5137a376d50e12078b5b0aa1edd032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e2c23581f17724f802",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "67996c9acd0e71f07e63",
+    "url": "/static/css/main.45ec8651.chunk.css"
+  },
+  {
+    "revision": "91e2c23581f17724f802",
+    "url": "/static/js/2.3a441d91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3a441d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67996c9acd0e71f07e63",
+    "url": "/static/js/main.34f52d05.chunk.js"
+  },
+  {
+    "revision": "3b9e881bac52614dff7b",
+    "url": "/static/js/runtime-main.538d85d3.js"
+  },
+  {
+    "revision": "8c91d1c6bfe2f51af80e1f6622256f60",
+    "url": "/static/media/BudgetTracker.8c91d1c6.jpg"
+  },
+  {
+    "revision": "f09d54279b235fde01e1fc05447e75f9",
+    "url": "/static/media/Lyngua.f09d5427.jpg"
+  },
+  {
+    "revision": "d8d2ff081ab41de8f1242f10a4cc5817",
+    "url": "/static/media/logo.d8d2ff08.png"
+  },
+  {
+    "revision": "67989073a3892e83fb03740d54de6017",
+    "url": "/static/media/me.67989073.png"
+  }
+]);
